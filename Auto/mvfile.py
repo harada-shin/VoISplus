447a44ea735066511f275c7626e205580056748d
harada-shin <44453694+harada-shin@users.noreply.github.com>
2018-11-29 15:24:55 +0900

    main

--- Auto/mvfile.py
+++
@@ -1,9 +0,0 @@
-from paramiko import SSHClient,AutoAddPolicy
-from scp import SCPClient
-
-ssh = SSHClient()
-ssh.set_missing_host_key_policy(AutoAddPolicy())
-ssh.connect(HOST, port=PORT, username=USER, password=PSWD)
-
-scp = SCPClient(ssh.get_transport())
-scp.get("/home/nextgen/NXS/conf/vliuser.ini")
